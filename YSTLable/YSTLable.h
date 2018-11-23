78b5d8f9f5ce404188da2f64365cdbc352f76fc1
杨书通 <>
2018-11-23 16:53:55 +0800

    提交

---
+++ YSTLable/YSTLable.h
@@ -0,0 +1,17 @@
+//
+//  YSTLable.h
+//  YSTLable
+//
+//  Created by 杨书通 on 2018/11/23.
+//  Copyright © 2018 杨书通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YSTLable : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YSTLable/YSTLable.m
@@ -0,0 +1,21 @@
+//
+//  YSTLable.m
+//  YSTLable
+//
+//  Created by 杨书通 on 2018/11/23.
+//  Copyright © 2018 杨书通. All rights reserved.
+//
+
+#import "YSTLable.h"
+
+@implementation YSTLable
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
